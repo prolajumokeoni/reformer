6b656faadbdfed986a3236d39075cee237ff2d5d
prolajumokeoni <prolajumokeoni@gmail.com>
2021-06-25 09:00:26 +0100

    Add user form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+    def index
+      @users = User.all
+    end
+    
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def new
+      @user = User.new
+    end
+
+  def create
+    @user = User.new(user_param)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  @user = User.find(params[:id])
+
+  if @user.update(user_param)
+    redirect_to @user
+  else
+    render :edit
+  end
+  end
+
+  def destroy 
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path
+  end
+  
+  def user_param            
+    params.require(:user).permit(:username, :email, :password)    #sanitize user input
+  end
+end
