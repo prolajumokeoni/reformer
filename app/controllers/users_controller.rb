66b667823e9a6a9864810a1ecead2217ef5557ad
prolajumokeoni <prolajumokeoni@gmail.com>
2021-06-25 17:58:25 +0100

    Add requested changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_param)
     if @user.save
-      redirect_to @user
+      redirect_to new_user_path
     else
       render :new
     end
